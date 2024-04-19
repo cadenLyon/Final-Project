b1cf01eb554e34cbc4fe7d64bf5b296474496954
Caden Lyon <lyon_caden@yahoo.com>
2024-04-18 19:18:16 -0600

    added user input stuff and vbox

--- FinalProject/src/main/java/com/example/finalproject/HelloApplication.java
+++ FinalProject/src/main/java/com/example/finalproject/HelloApplication.java
@@ -2,7 +2,19 @@ package com.example.finalproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +22,51 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group group = new Group();
+
+
+        VBox vBox = new VBox();
+        vBox.getChildren().add(addVBox());
+        //vBox.setAlignment(Pos.CENTER);
+
+
+        stage.setTitle("NAME TO BE MADE");
+        Scene scene = new Scene(group, 1150, 647,Color.web("0077B6"));
+        group.getChildren().addAll(vBox);
+        double x = (scene.getWidth() - vBox.prefWidth(-1))/2;
+
+        vBox.setLayoutX(x);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public VBox addVBox() {
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(100));
+        vBox.setSpacing(30);
+        vBox.setAlignment(Pos.CENTER);
+
+
+        Text userNameText = new Text("Enter Username");
+        userNameText.setFont(Font.font("Verdana", 20));
+
+        TextField userNameInput = new TextField();
+
+        Text passWordText = new Text("Enter Password");
+        passWordText.setFont(Font.font("Verdana", 20));
+
+
+        TextField passWordInput = new TextField();
+
+        Button signInButton = new Button("Sign in");
+        Button createAccountButton = new Button("Create an account");
+
+        vBox.getChildren().addAll(userNameText,userNameInput,passWordText,passWordInput,signInButton,createAccountButton);
+
+
+
+
+        return vBox;
     }
 
     public static void main(String[] args) {
